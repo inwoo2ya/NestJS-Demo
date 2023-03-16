6f5ed5b1ea25d050c8ba50dd384f3b9aa2f427ef
lavarwave.inwoo <liw0627@lavar.co.kr>
2023-03-16 17:56:12 +0900

    [2023.03.16] [이인우][feat]

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,15 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
-import { Board } from './board.model';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 
@@ -13,6 +23,7 @@ export class BoardsController {
   }
 
   @Post('/')
+  @UsePipes(ValidationPipe)
   createBoard(@Body() createBoardDto: CreateBoardDto): Board {
     return this.boardsService.createBoards(createBoardDto);
   }
@@ -26,6 +37,15 @@ export class BoardsController {
   deleteBoard(@Param('id') id: string): void {
     this.boardsService.deleteBoard(id);
   }
+
+  @Patch('/:id/status')
+  updateBoardStatus(
+    @Param('id') id: string,
+    @Body('status') status: BoardStatus,
+  ) {
+    return this.boardsService.updateBoardStatus(id, status);
+  }
+
   //위에는 타입스크립트 전용 밑에는 자바스크립트 (private,protect,public... only typescript)
   //boardsService: BoardsService;
   //constructor(boardsService: BoardsService){ 파라미터
